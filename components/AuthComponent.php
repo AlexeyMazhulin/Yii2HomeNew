2165a2162dd6af3d8b5c29584b55c0b0b5f3bc45
alexeyMazhulin <a.mazhulin@mail.ru>
2019-05-03 11:34:56 +0300

    0305

--- components/AuthComponent.php
+++ components/AuthComponent.php
@@ -4,7 +4,17 @@
 namespace app\components;
 
 
-class AuthComponent
+use app\models\Users;
+use yii\base\Component;
+
+class AuthComponent extends Component
 {
+    public function getModel(){
+        return new Users();
+    }
+
+    public function createUser(&$model):bool{
+
+    }
 
 }
--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -4,7 +4,23 @@
 namespace app\controllers;
 
 
-class AuthController
-{
+use app\components\AuthComponent;
+use yii\web\Controller;
 
+class AuthController extends Controller
+{
+    public $component;
+
+    public function init()
+    {
+        parent::init();
+
+        $this->component=\Yii::createObject(['class'=>AuthComponent::class]);
+    }
+
+    public function actionSignUp(){
+        $model=$this->component->getModel();
+
+        return $this->render('signup',['model'=>$model]);
+    }
 }
